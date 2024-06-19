004acda1f47f503cb59c871eaa1ae69a8ef6cad4
siarhkul <siarhkul@gmail.com>
2024-06-19 14:42:02 +0200

    Socet

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ mongoose
         'mongodb+srv://siarhkul:12345@nodecoursecluster.1sfoxnj.mongodb.net/?retryWrites=true&w=majority&appName=NodeCourseCluster'
     )
     .then(result => {
-      app.listen(8080);
+      const server = app.listen(8080);
+      const io = require('./socket').init(server);
+      io.on('connection', socket => {
+        console.log('Client connected');
+      });
     })
     .catch(err => console.log(err));
